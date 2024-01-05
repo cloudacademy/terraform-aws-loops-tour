16547468e9b640e41662102110984b0de3cb0469
Jun <jun.fritz@gmail.com>
2024-01-04 14:04:33 -1000

    tour steps.

---
+++ src/modules/ec2/variables.tf
@@ -0,0 +1,16 @@
+variable "associate_public_ip_address" {
+  description = "Associate a public IP address with EC2 instance"
+  type        = bool
+  default     = true
+}
+
+variable "servername"{
+    description = "Name of the server"
+    type = string
+}
+
+variable "ebs_block_device" {
+  description = "Additional EBS block devices to attach to the instance"
+  type        = list(map(string))
+  default     = []
+}
